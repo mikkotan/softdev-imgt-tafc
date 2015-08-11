30199b9c247eb77d97d05648b63b9a0a258d0631
Anfernee Joan Ng <starjirachi1@yahoo.com>
2015-08-11 16:25:24 +0800

    tinloa lhog michael

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,10 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_name, :password, :role)
+  end
 end
