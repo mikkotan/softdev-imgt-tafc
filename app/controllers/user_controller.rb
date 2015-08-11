f07eac86f80477511d6df0754eb251ae19c8474d
Anfernee Joan Ng <starjirachi1@yahoo.com>
2015-08-11 16:02:33 +0800

    Rehashed the whole thing

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+  def view
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'user/view'
 
-  resources :users
+  get 'user/new'
 
-  resources :employees
+  get 'user/index'
+
+  get 'user/edit'
+
+  get 'user/view'
+
+  get 'user/index'
+
+  get 'user/new'
+
+  get 'user/edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
