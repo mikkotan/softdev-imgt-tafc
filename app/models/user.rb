15477f8f9f91754a8b5f456864cf80c77c28af85
starjirachi1 <starjirachi1@yahoo.com>
2015-08-19 11:44:50 +0800

    made some changes to feature test cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :password, length: { minimum: 8 }
+  validates :username, length: { in: 6..20 }
 
   attr_accessor :password
   before_save :encrypt_password
