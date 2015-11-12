f5ffdb7da744364cddfe94a6d707e414ee2844d5
Michael H. Tan <mikkoyunltd@gmail.com>
2015-11-12 13:29:16 +0800

    Minor updates in Transaction Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817131904) do
+ActiveRecord::Schema.define(version: 20151007150542) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "company_name"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20150817131904) do
   end
 
   add_index "clients", ["user_id"], name: "index_clients_on_user_id"
+
+  create_table "transactions", force: :cascade do |t|
+    t.datetime "month_and_year"
+    t.float    "retainers_fee"
+    t.float    "vat"
+    t.float    "percentage"
+    t.string   "other_processing"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
