12bc4189b69af7e66ccc3642326880b06bce8b0f
Albert <ambetaf@gmail.com>
2015-11-21 19:20:25 +0800

    Added Kaminari for pagination. Paginated Users#index and Employees#index

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -99,98 +99,87 @@ RSpec.feature 'User CRUD thingy', js: true do
   # Dear Albert, paki-edit lg hahaha. Ang minimum length sg password 8 characters.
   # Kg ang minimum length sg username 6 characters.
 
-  # before(:each) do
-  #   User.create(password: 'pulitzer',
-  #               username: 'starjirachi1',
-  #               password_confirmation: 'pulitzer',
-  #               first_name: 'Anfernee',
-  #               last_name: 'Ng',
-  #               role: 'owner')
-  #
-  #   User.create(password: 'wow',
-  #               username: 'aafuensalida',
-  #               password_confirmation: 'wow',
-  #               first_name: 'Albert',
-  #               last_name: 'Fuensalida',
-  #               role: 'employee')
-  # end
-  #
-  # scenario 'Owner creates an Employee' do
-  #   visit '/login'
-  #   fill_in 'username', with: 'starjirachi1'
-  #   fill_in 'password', with: 'pulitzer'
-  #
-  #   click_button 'Log in'
-  #   visit '/users/new'
-  #
-  #   fill_in 'user_first_name', with: 'Keia Joy'
-  #   fill_in 'user_last_name', with: 'Harder'
-  #   fill_in 'user_username', with: 'keia123'
-  #   fill_in 'user_password', with: 'aaaa'
-  #   fill_in 'user_password_confirmation', with: 'aaaa'
-  #   choose 'user_role_owner'
-  #   click_button 'Create User'
-  #
-  #   expect(page).to have_text 'User successfully created.'
-  # end
-  #
-  # scenario 'Owner edits Employee' do
-  #   visit '/login'
-  #   fill_in 'username', with: 'starjirachi1'
-  #   fill_in 'password', with: 'pulitzer'
-  #
-  #   click_button 'Log in'
-  #   visit '/users/2/edit'
-  #
-  #   fill_in 'user_first_name', with: 'Albert'
-  #   fill_in 'user_last_name', with: 'Fuensalida'
-  #   fill_in 'user_username', with: 'ambet_123'
-  #   choose 'user_role_owner'
-  #   click_button 'Update User'
-  #   expect(page).to have_text 'Successfully updated profile.'
-  # end
-  #
-  # scenario 'Owner uses logout' do
-  #   visit '/login'
-  #   fill_in 'username', with: 'starjirachi1'
-  #   fill_in 'password', with: 'pulitzer'
-  #   click_button 'Log in'
-  #   visit '/logout'
-  #   expect(page).to have_text 'Successfully Logged Out!'
-  # end
-  #
-  # scenario 'User registers' do
-  #   visit '/users/new'
-  #
-  #   fill_in 'user_first_name', with: 'Albert Jr.'
-  #   fill_in 'user_last_name', with: 'Fuensalida'
-  #   fill_in 'user_username', with: 'aafuensalida'
-  #   fill_in 'user_password', with: 'amoroso'
-  #   fill_in 'user_password_confirmation', with: 'amoroso'
-  #   choose 'user_role_manager'
-  #   click_button 'Create User'
-  #   expect(page).to have_text 'User successfully created.'
-  # end
-  #
-  # scenario 'User inputs wrong password' do
-  #   visit '/login'
-  #   fill_in 'username', with: 'albert'
-  #   fill_in 'password', with: 'wrongpassword'
-  #   click_button 'Log in'
-  #   expect(page).to have_text 'Invalid Username or Password!'
-  # end
+  before(:each) do
+    User.create(password: 'pulitzer',
+                email: 'starjirachi1@yahoo.com',
+                password_confirmation: 'pulitzer',
+                first_name: 'Anfernee',
+                last_name: 'Ng',
+                role: 'owner')
 
-  # scenario 'Owner edits an employee with wrong password confirmation' do
-  #   visit '/login'
-  #   fill_in 'username', with: 'starjirachi1'
-  #   fill_in 'password', with: 'pulitzer'
-  #
-  #   click_button 'Log in'
-  #   visit '/users/2/edit'
-  #   fill_in 'user_password', with: 'ambet 123'
-  #   fill_in 'user_password_confirmation', with: 'wrongpassword'
-  #   click_button 'Update User'
-  #
-  #   expect(page).to have_text 'Password confirmation'
-  # end
+    User.create(password: 'wow1234567',
+                email: 'aafuensalida@yahoo.com',
+                password_confirmation: 'wow1234567',
+                first_name: 'Albert',
+                last_name: 'Fuensalida',
+                role: 'employee')
+  end
+
+  scenario 'Owner creates an Employee' do
+    visit '/login'
+    fill_in 'email', with: 'starjirachi1@yahoo.com'
+    fill_in 'password', with: 'pulitzer'
+
+    click_button 'Log in'
+    visit '/users/new'
+
+    fill_in 'user_first_name', with: 'Keia Joy'
+    fill_in 'user_last_name', with: 'Harder'
+    fill_in 'user_email', with: 'keia123@yahoo.com'
+    fill_in 'user_password', with: 'IloveNongArms'
+    fill_in 'user_password_confirmation', with: 'IloveNongArms'
+    choose 'user_role_employee'
+    click_button 'Create User'
+
+    expect(page).to have_text 'User successfully created.'
+  end
+
+  scenario 'Owner edits Employee' do
+    visit '/login'
+    fill_in 'email', with: 'starjirachi1@yahoo.com'
+    fill_in 'password', with: 'pulitzer'
+
+    click_button 'Log in'
+    visit '/users/2/edit'
+
+    fill_in 'user_first_name', with: 'Albert'
+    fill_in 'user_last_name', with: 'Fuensalida'
+    fill_in 'user_email', with: 'ambet_123@yahoo.com'
+    choose 'user_role_employee'
+    click_button 'Update User'
+    expect(page).to have_text 'Successfully updated profile.'
+  end
+
+  scenario 'Owner uses logout' do
+    visit '/login'
+    fill_in 'email', with: 'starjirachi1@yahoo.com'
+    fill_in 'password', with: 'pulitzer'
+    click_button 'Log in'
+    visit '/logout'
+    expect(page).to have_text 'Successfully Logged Out!'
+  end
+
+
+  scenario 'User inputs wrong password' do
+    visit '/logout'
+    visit '/login'
+    fill_in 'email', with: 'aafuensalida@yahoo.com'
+    fill_in 'password', with: 'wrongpassword'
+    click_button 'Log in'
+    expect(page).to have_text 'Invalid Email or Password!'
+  end
+
+  scenario 'Owner edits an employee with wrong password confirmation' do
+    visit '/login'
+    fill_in 'email', with: 'starjirachi1@yahoo.com'
+    fill_in 'password', with: 'pulitzer'
+
+    click_button 'Log in'
+    visit '/users/2/change_password'
+    fill_in 'user_password', with: 'ambet_1234567'
+    fill_in 'user_password_confirmation', with: 'wrongpassword'
+    click_button 'Update User'
+
+    expect(page).to have_text 'Password confirmation'
+  end
 end
