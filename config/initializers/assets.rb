21d574f898541117ecadea7c6ea96a9022dcb38d
Albert <ambetaf@gmail.com>
2016-01-03 07:53:31 +0800

    icomoon custom icon logo added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.sass, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.config.assets.precompile += %w( custom-icons.eot )
+Rails.application.config.assets.precompile += %w( custom-icons.ttf )
+Rails.application.config.assets.precompile += %w( custom-icons.woff )
+Rails.application.config.assets.precompile += %w( custom-icons.svg )
