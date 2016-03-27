140147238afa8f3f05a095040187b9fe7a776e54
Anfernee Joan Ng <starjirachi1@yahoo.com>
2016-03-27 20:30:56 +0800

    unauthorized works well :D

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :save_current_url
 
-  helper_method :employees
+  helper_method :get_employees
   helper_method :current_user
 
   rescue_from CanCan::AccessDenied do |_exception|
     flash[:alert] = 'You are unauthorized!'
-    redirect_to '/login'
+    redirect_to session.delete(:return_to)
   end
 
   private
+  def save_current_url
+    session[:return_to] = request.referer
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
